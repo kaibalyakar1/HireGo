185fb2e3cf04160c8cfa71433fc071da3b36b242
kaibalyakar1 <kaibalyakar95@gmail.com>
2024-07-19 21:29:16 +0530

    Files created

---
+++ Backend/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
+const app = express();
+dotenv.config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+export default app;
