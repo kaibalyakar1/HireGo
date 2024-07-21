34ead859561f88fbddbee1f016dd76ce6711712f
kaibalyakar1 <kaibalyakar95@gmail.com>
2024-07-22 00:04:58 +0530

    application model done

---
+++ Backend/controllers/application.controller.js
@@ -0,0 +1,3 @@
+import catchAsyncError from "../middlewares/catchAsyncError.js";
+import Application from "../models/application.models.js";
+import ErrorHandler from "../middlewares/error.js";
--- Backend/models/application.models.js
+++ Backend/models/application.models.js
@@ -1,4 +1,63 @@
 import mongoose from "mongoose";
+import validator from "validator";
+const application = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your Name!"],
+      minLength: [3, "Name must contain at least 3 Characters!"],
+      maxLength: [30, "Name cannot exceed 30 Characters!"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your Email!"],
+      validate: [validator.isEmail, "Please provide a valid Email!"],
+    },
+    phone: {
+      type: Number,
+      required: [true, "Please enter your Phone Number!"],
+    },
+    coverLetter: {
+      type: String,
+      required: [true, "Please enter your Cover Letter!"],
+    },
+    resume: {
+      public_id: {
+        type: String,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+    },
+    applicantId: {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
 
-const application = new mongoose.Schema({}, { timestamps: true });
+      role: {
+        type: String,
+        enum: ["Job Seeker"],
+        required: true,
+      },
+    },
+
+    emplpoyerId: {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+
+      role: {
+        type: String,
+        enum: ["Employer"],
+        required: true,
+      },
+    },
+  },
+  { timestamps: true }
+);
 export default mongoose.model("Application", application);
